173f9536b074eac46ff0e08107a2c235458a09e3
aLoneMass <mapcer@gmail.com>
2025-04-06 22:44:10 +0300

    0,5

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
-from bot.handlers import start
-from bot.handlers import start, schedule
+from bot.handlers import start, schedule, registration
 
 async def main():
     bot = Bot(token=BOT_TOKEN)
@@ -11,6 +10,7 @@ async def main():
     # Здесь будут хендлеры и роутеры
     dp.include_router(start.router)
     dp.include_router(schedule.router)
+    dp.include_router(registration.router)
     print('Бот запущен')
     await dp.start_polling(bot)
 
