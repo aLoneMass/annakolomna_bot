5a08289ccbae19853a1c559064fc34100c5d253c
aLoneMass <mapcer@gmail.com>
2025-04-13 18:23:29 +0300

    0.39

--- bot/states/registration.py
+++ bot/states/registration.py
@@ -2,10 +2,13 @@ from aiogram.fsm.state import StatesGroup, State
 
 class RegistrationState(StatesGroup):
     choosing_event = State()
-    confirming_child = State()  # 🆕 новое состояние
+    confirming_child = State()  
     entering_child_name = State()
     entering_allergy_info = State()
     confirming_comment = State()
+    entering_child_age = State()  # 🆕 новое состояние
     waiting_for_payment_check = State()
+    
+
     
 
